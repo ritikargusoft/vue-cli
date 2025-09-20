27626d6d38b5eaaa6ae992e4cbd2f0bf0c456ae4
Ritik Negi <negiritik47@gmail.com>
2025-09-20 17:44:33 +0530

    emitting custom events

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : ''}}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : ''}}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -20,6 +20,10 @@
 export default {
   // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
   props: {
+    id: {
+      type: String,
+      required: true
+    },
     name: {
       type: String,
       required: true,
@@ -43,8 +47,7 @@ export default {
   },
   data() {
     return {
-      detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite,
+      detailsAreVisible: false
     };
   },
   methods: {
@@ -52,7 +55,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id);
     },
   },
 };
